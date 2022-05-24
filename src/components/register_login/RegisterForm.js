2d4f1ac6940a3023aae58b43c57cf4fa7081cf46
Luan Severo <luansevero@icloud.com>
2022-05-24 16:53:08 -0300

    Registration structure

--- src/components/register_login/RegisterForm.js
+++ src/components/register_login/RegisterForm.js
@@ -1,45 +1,73 @@
 import { Link } from "react-router-dom"
-import { Container } from "./style"
-import { useContext } from "react"
+import axios from "axios"
+import { useContext, useState } from "react"
+import { useNavigate } from "react-router"
 
 import TokenContext from "../../contexts/TokenContext"
 
+import { Container } from "./style"
+
 export default function RegisterForm() {
+    const [formData, setFormData] = useState({
+        email: "",
+        name: "",
+        image: "",
+        password: ""
+    });
+    const navigate = useNavigate();
+
+    function handleRegistration(e){
+        const promisse = axios.post(
+            "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up", 
+            formData
+        );
+
+        promisse.then(navigate("/"));
+        promisse.catch(alert("Registro não foi efetuado com sucesso"))
+    }
+
+    function InputChange(e){
+        setFormData({...formData, [e.target.name]: e.target.value });
+    }
 
     return (
         <Container>
-            <form onSubmit="Registrado">
+            <form onSubmit={handleRegistration}>
                 <label for="emailInput"></label>
                 <input
                     type="email"
                     id="emailInput"
                     placeholder="email"
-                    value="userEmail"
-                    /*onChange={e => x}*/>
+                    value={formData.email}
+                    name="email"
+                    onChange={InputChange}>
                 </input>
                 <label for="passwordInput"></label>
                 <input
                     type="password"
                     id="passwordInput"
                     placeholder="senha"
-                    value="userPassword"
-                    /*onChange={e => x}*/>
+                    value={formData.password}
+                    name="password"
+                    onChange={InputChange}>
                 </input>
                 <label for="usernameInput"></label>
                 <input
                     type="text"
                     id="usernameInput"
                     placeholder="nome"
-                    value="username"
-                    /*onChange={e => x}*/>
+                    value={formData.name}
+                    name="name"
+                    onChange={InputChange}>
                 </input>
                 <label for="userPicInput"></label>
                 <input
-                    type="image"
+                    type="text"
                     id="userPicInput"
                     placeholder="foto"
-                    value="userPic"
-                    /*onChange={e => x}*/>
+                    value={formData.image}
+                    name="image"
+                    onChange={InputChange}>
                 </input>
                 <button type="submit">Cadastrar</button>
             </form>
